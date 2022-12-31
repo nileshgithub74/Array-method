bad05731ee924b43637f817539c58740328d01e5
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2022-12-31 18:02:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// let num = [1, 3, 45, 34, 24, 45, 43]
+// let b = num.toString()
+// console.log(b)
+// let c = num.join(" and ") 
+// console.log(c, typeof c)
+// let r = num.pop()
+// console.log(r)
+
+let num = [1, 53, 455, 34, 10004, 455, 543]
+num.sort()
+console.log(num)
